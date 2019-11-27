d2f22787b790a5c8c2d4c469764dcd535ba1eaf4
sebastian <sebaxtian098@hotmail.com>
2019-11-27 17:59:46 -0500

    ejercicios de logica

--- holamundo/src/index.js
+++ holamundo/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import HolaMundo from './components/HolaMundo';
+import Button from './components/Button';
 import './index.css';
 
+
 ReactDOM.render(
-  <HolaMundo />,
+  <Button  text="Click:_2" />,
   document.getElementById('root')
 );
